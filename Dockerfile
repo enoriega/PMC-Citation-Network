7469874a2d517feb13e6cc400c829e8e2ad94c56
Enrique Noriega <enoriega@arizona.edu>
2025-08-27 11:49:05 -0700

    Added bulk db creation script for the container

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,6 @@ COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 
-# Default output file name
-ENV OUT_FILE="out.json"
-
 # Copy the project into the image
 ADD . /app
 # Set work directory
@@ -16,6 +13,6 @@ WORKDIR /app
 # Install the virtual environment
 RUN uv sync --locked
 
-# Run the
+# Run the xml processing by default
 CMD ["/bin/sh", "/app/process_directories.sh"]
 
---
+++ bulk_populate_db.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cd /app
+
+INPUT_DIR=/input
+
+# Concatenate all files into single jsonl
+echo "Concatenating files"
+find "${INPUT_DIR}" -maxdepth 1 -type f -name '*.jsonl' | while read -r file; do
+	cat "$file" >> "${INPUT_DIR}/temp.jsonl"
+	echo "$(basename "$file")" >> "${INPUT_DIR}/processed_files.txt"
+done
+
+# Run bulk creation
+echo "Importing data into $CONN_STRING"
+uv run citation_network.py populate-database ${CONN_STRING} "${INPUT_DIR}/temp.jsonl" --overwrite-file
+
+# Remove temp file
+rm "${INPUT_DIR}/temp.jsonl"
