e34e5759ab06f63e588aad73569820471cb7de6e
Enrique Noriega <enoriega@arizona.edu>
2025-08-29 11:02:43 -0700

    Fixed the app command in `incremental_populate_db.s`h`

--- incremental_populate_db.sh
+++ incremental_populate_db.sh
@@ -7,9 +7,11 @@ INPUT_DIR=/input
 echo "Incrementally adding data into $CONN_STRING"
 find "${INPUT_DIR}" -maxdepth 1 -type f -name '*.jsonl' | while read -r file; do
 	file_name=$(basename "$file") 
+	# Compute the number of lines, if the directory didn't have 
+	num_lines=$(wc -l < "$file")
 	if [ ! -f "${INPUT_DIR}/processed_files.txt" ] || ! grep -Fxq "${file_name}" "${INPUT_DIR}/processed_files.txt"; then
 		echo "Importing from ${file_name}"
-		uv run citation_network.py populate-database "${CONN_STRING}" "${file}" --overwrite-file
+		uv run citation_network.py add-data "${CONN_STRING}" "${file}"
 		echo "$file_name" >> "${INPUT_DIR}/processed_files.txt"
 	fi
 done
